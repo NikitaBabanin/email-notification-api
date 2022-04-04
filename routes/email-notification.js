846f5069e9f07833f0882574d5b7ae183daa3f9b
NikitaBabanin <postmanngmilus@gmail.com>
2022-04-04 09:44:06 +0300

    added resend url

--- routes/email-notification.js
+++ routes/email-notification.js
@@ -13,7 +13,7 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-router.post('/send', async (req, res) => {
+router.post('/resend', async (req, res) => {
     const {email} = req.body
 
     if (!email) {
@@ -25,6 +25,22 @@ router.post('/send', async (req, res) => {
         res.json({value: true, message: 'Message sent'})
     } catch (error) {
         res.json({value: false, message: error.message})
+    }
+})
+router.post('/send', async (req, res) => {
+    const email = '';
+    const properties = req.body.line_items[0].properties
+
+    properties.forEach((item) =>{
+        if(item.key === 'Email'){
+            email: item.value;
+        }
+    })
+    try {
+        await transporter.sendMail(standartNotification(email))
+        res.json({value: true, message: 'Message sent', body:req.body})
+    } catch (error) {
+        res.json({value: false, message: error.message,body:req.body})
     }
 })
 
